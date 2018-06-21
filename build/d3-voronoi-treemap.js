dda3ee97cbcb69eb97448fa2b755914f72c5a275
Aleksandar <bobic.aleksandar92@gmail.com>
2018-06-21 22:17:58 +0200

    Add max recursion property

--- build/d3-voronoi-treemap.js
+++ build/d3-voronoi-treemap.js
@@ -9,6 +9,7 @@
     var DEFAULT_CONVERGENCE_RATIO = 0.01;
     var DEFAULT_MAX_ITERATION_COUNT = 50;
     var DEFAULT_MIN_WEIGHT_RATIO = 0.01;
+    var DEFAULT_MAX_RECURSION_ITER = 5;
     //end: constants
 
     /////// Inputs ///////
@@ -17,6 +18,7 @@
     var maxIterationCount = DEFAULT_MAX_ITERATION_COUNT;  // maximum allowed iteration; stops computation even if convergence is not reached; use a large amount for a sole converge-based computation stop
     var minWeightRatio = DEFAULT_MIN_WEIGHT_RATIO;        // used to compute the minimum allowed weight; default 0.01 means 1% of max weight; handle near-zero weights, and leaves enought space for cell hovering
 
+    var maxRecursionIter = DEFAULT_MAX_RECURSION_ITER;
     //begin: internals
     var _voronoiMap = d3VoronoiMap.voronoiMap();
     //end: internals
@@ -64,18 +66,30 @@
       return _voronoiTreemap;
     };
 
+    _voronoiTreemap.maxRecursionIter = function (_) {
+      if (!arguments.length) { return maxRecursionIter; }
+
+      maxRecursionIter = _;
+      return _voronoiTreemap;
+    };
     ///////////////////////
     /////// Private ///////
     ///////////////////////
 
-    function recurse(clippingPolygon, node) {
+    function recurse(clippingPolygon, node, iter = 0) {
       var voronoiMapRes;
       var originalData = [];
+      var end = false;
 
       //assign polygon to node
       node.polygon = clippingPolygon;
+      if(maxRecursionIter && iter >= maxRecursionIter) {
+        end = true;
+      } else {
+        iter = iter + 1;
+      }
 
-      if (node.height!=0) {
+      if (node.height!=0 && !end) {
         //compute one-level Voronoi map of children
         voronoiMapRes = _voronoiMap.clip(clippingPolygon)(node.children ? node.children : node);
 
@@ -84,7 +98,7 @@
 
         //begin: recurse on children
         voronoiMapRes.polygons.forEach(function(cp){
-          recurse(cp, cp.site.originalObject.data.originalData);
+          recurse(cp, cp.site.originalObject.data.originalData, iter);
         })
         //end: recurse on children
       }
--- src/d3-voronoi-treemap.js
+++ src/d3-voronoi-treemap.js
@@ -5,6 +5,7 @@ export function voronoiTreemap () {
   var DEFAULT_CONVERGENCE_RATIO = 0.01;
   var DEFAULT_MAX_ITERATION_COUNT = 50;
   var DEFAULT_MIN_WEIGHT_RATIO = 0.01;
+  var DEFAULT_MAX_RECURSION_ITER = 5;
   //end: constants
 
   /////// Inputs ///////
@@ -13,6 +14,7 @@ export function voronoiTreemap () {
   var maxIterationCount = DEFAULT_MAX_ITERATION_COUNT;  // maximum allowed iteration; stops computation even if convergence is not reached; use a large amount for a sole converge-based computation stop
   var minWeightRatio = DEFAULT_MIN_WEIGHT_RATIO;        // used to compute the minimum allowed weight; default 0.01 means 1% of max weight; handle near-zero weights, and leaves enought space for cell hovering
 
+  var maxRecursionIter = DEFAULT_MAX_RECURSION_ITER;
   //begin: internals
   var _voronoiMap = voronoiMap();
   //end: internals
@@ -60,18 +62,30 @@ export function voronoiTreemap () {
     return _voronoiTreemap;
   };
 
+  _voronoiTreemap.maxRecursionIter = function (_) {
+    if (!arguments.length) { return maxRecursionIter; }
+
+    maxRecursionIter = _;
+    return _voronoiTreemap;
+  };
   ///////////////////////
   /////// Private ///////
   ///////////////////////
 
-  function recurse(clippingPolygon, node) {
+  function recurse(clippingPolygon, node, iter = 0) {
     var voronoiMapRes;
     var originalData = [];
+    var end = false;
 
     //assign polygon to node
     node.polygon = clippingPolygon;
+    if(maxRecursionIter && iter >= maxRecursionIter) {
+      end = true;
+    } else {
+      iter = iter + 1;
+    }
 
-    if (node.height!=0) {
+    if (node.height!=0 && !end) {
       //compute one-level Voronoi map of children
       voronoiMapRes = _voronoiMap.clip(clippingPolygon)(node.children ? node.children : node);
 
@@ -80,7 +94,7 @@ export function voronoiTreemap () {
 
       //begin: recurse on children
       voronoiMapRes.polygons.forEach(function(cp){
-        recurse(cp, cp.site.originalObject.data.originalData);
+        recurse(cp, cp.site.originalObject.data.originalData, iter);
       })
       //end: recurse on children
     }
